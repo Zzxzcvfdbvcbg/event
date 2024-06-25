292840fdd8892c5855cd26d28cb7160725015cd6
Zzxzcvfdbvcbg <z2201210269@outlook.com>
2024-06-25 17:29:11 +0800

    个人中心

--- src/api/user.js
+++ src/api/user.js
@@ -26,3 +26,13 @@ export const userGetInfo = () => {
     url: '/my/userinfo'
   })
 }
+
+// 个人信息
+export const userUpdateInfoService = ({ id, nickname, email }) =>
+  request.put('/my/userinfo', { id, nickname, email })
+// 更换图像
+export const userUploadAvatarService = (avatar) =>
+  request.patch('/my/update/avatar', { avatar })
+// 重置密码
+export const userUpdatePassService = ({ old_pwd, new_pwd, re_pwd }) =>
+  request.patch('/my/updatepwd', { old_pwd, new_pwd, re_pwd })
